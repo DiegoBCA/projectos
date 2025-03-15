a3021f6b3c1631317bffd7b907e46a9b085fc47f
SantiagoLopezAldana <santiago061810@gmail.com>
2025-03-15 14:37:04 -0600

    Update App.java

--- project/src/App.java
+++ project/src/App.java
@@ -1,13 +1,5 @@
-import javax.swing.JFrame;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Cliente frame2 = new Cliente();
-        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        Administrador frame = new Administrador();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 200);
-        frame.setVisible(true);
+        new Cliente();
     }
 }
